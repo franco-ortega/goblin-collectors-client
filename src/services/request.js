fa97e85f11da366034374aa7712eb1e5a5bbe155
Franco Ortega <francopdx@gmail.com>
2022-08-04 12:57:33 -0700

    remove commented out code

--- src/services/request.js
+++ src/services/request.js
@@ -3,34 +3,3 @@ const { REACT_APP_API_URL } = process.env;
 export const getGoblins = async () => {
   await fetch(REACT_APP_API_URL).then((res) => res.json());
 };
-
-// const WITHOUT_BODY_METHODS = ['GET', 'DELETE'];
-
-// const request = (path, method, body) => {
-//   const headers = WITHOUT_BODY_METHODS.includes(method)
-//     ? {}
-//     : {
-//         'Content-Type': 'application/json'
-//       };
-
-//   return fetch(`${REACT_APP_API_URL}${path}`, {
-//     method,
-//     headers,
-//     credentials: 'include',
-//     body: JSON.stringify(body)
-//   }).then((res) => {
-//     // console.log(res);
-//     return res;
-//   });
-//   // .then((res) => Promise.all([res.ok, res.json()]))
-//   // .then(([ok, json]) => {
-//   //   console.log(json);
-//   //   if (!ok) throw json;
-//   //   return json;
-//   // });
-// };
-
-// export const postGoblin = (path, body) => request(path, 'POST', body);
-// export const getGoblins = (path) => request(path, 'GET');
-// export const putGoblin = (path, body) => request(path, 'PUT', body);
-// export const deleteGoblin = (path) => request(path, 'DELETE');
